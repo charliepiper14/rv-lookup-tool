06749b9e02b0e6cd79dab14f5431cea04ac30b85
charliepiper14 <charles@leadsguys.co.uk>
2025-06-06 14:28:16 +0700

    Add files via upload

--- fetch-valuation.js
+++ fetch-valuation.js
@@ -1,6 +1,7 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
+
 module.exports = async (req, res) => {
   const { id } = req.query;
   const html = await axios.get(`https://www.tax.service.gov.uk/business-rates-find/valuations/start/${id}`).then(r => r.data);
@@ -8,6 +9,7 @@ module.exports = async (req, res) => {
   const rateable_value = $('dt:contains("Rateable value")').next().text().trim();
   const description = $('dt:contains("Property description")').next().text().trim();
   const address = $('h1').text().trim();
+
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json({ address, description, rateable_value });
 };
--- lookup-addresses.js
+++ lookup-addresses.js
@@ -1,12 +1,14 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
+
 module.exports = async (req, res) => {
   const { postcode } = req.query;
   const searchUrl = `https://www.tax.service.gov.uk/business-rates-find/search?postcode=${encodeURIComponent(postcode)}`;
   const html = await axios.get(searchUrl).then(r => r.data);
   const $ = cheerio.load(html);
   const addresses = [];
+
   $('a.govuk-link').each((_, el) => {
     const href = $(el).attr('href');
     const text = $(el).text().trim();
@@ -15,6 +17,7 @@ module.exports = async (req, res) => {
       addresses.push({ id: match[1], address: text });
     }
   });
+
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json(addresses);
 };

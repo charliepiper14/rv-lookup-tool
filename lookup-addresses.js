9a4d447034c04f9218c5425b17a4504cea826593
charliepiper14 <charles@leadsguys.co.uk>
2025-06-06 19:31:53 +0700

    Add files via upload

--- lookup-addresses.js
+++ lookup-addresses.js
@@ -1,19 +1,37 @@
 
-const axios = require('axios');
-const cheerio = require('cheerio');
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const { postcode } = req.query;
-  const url = `https://www.tax.service.gov.uk/business-rates-find/search?postcode=${encodeURIComponent(postcode)}`;
-  const html = await axios.get(url).then(r => r.data);
-  const $ = cheerio.load(html);
-  const results = [];
-  $('a.govuk-link').each((i, el) => {
-    const href = $(el).attr('href');
-    const match = href.match(/\/valuations\/start\/(\d+)/);
-    if (match) {
-      results.push({ id: match[1], address: $(el).text().trim() });
-    }
-  });
-  res.status(200).json(results);
-};
+
+  if (!postcode) {
+    return res.status(400).json({ error: "Missing postcode" });
+  }
+
+  const searchUrl = `https://www.tax.service.gov.uk/business-rates-find/search?postcode=${encodeURIComponent(postcode)}`;
+
+  try {
+    const html = await axios.get(searchUrl, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Rateable Value Tool)'
+      }
+    }).then(r => r.data);
+
+    const $ = cheerio.load(html);
+    const results = [];
+
+    $('a.govuk-link').each((i, el) => {
+      const href = $(el).attr('href');
+      const idMatch = href.match(/\/valuations\/start\/(\d+)/);
+      if (idMatch) {
+        results.push({ id: idMatch[1], address: $(el).text().trim() });
+      }
+    });
+
+    return res.status(200).json(results);
+  } catch (err) {
+    console.error("Error fetching address list", err);
+    return res.status(500).json({ error: "Scraping failed" });
+  }
+}

50731fa0b91c236128651df72bb0c34009f658fd
charliepiper14 <charles@leadsguys.co.uk>
2025-06-09 09:04:42 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-
-document.getElementById('form-step-1').addEventListener('submit', function(e) {
-  e.preventDefault();
-  document.getElementById('step-1').classList.remove('active');
-  document.getElementById('step-2').classList.add('active');
-});
-
-document.getElementById('to-step-3').addEventListener('click', function() {
-  document.getElementById('step-2').classList.remove('active');
-  document.getElementById('step-3').classList.add('active');
-  document.getElementById('results').innerText = 'Rateable value results loading...';
-});
+// script.js placeholder

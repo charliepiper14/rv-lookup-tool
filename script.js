48e3d2fb80cb93e99cf853e6f122211717afd057
charliepiper14 <charles@leadsguys.co.uk>
2025-06-13 15:38:50 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById("nextStepBtn").addEventListener("click", () => {
-    document.getElementById("step1").style.display = "none";
-    document.getElementById("step2").style.display = "block";
-});
